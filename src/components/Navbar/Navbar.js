8ac2a9b089e49b0034f8f33f998acd226bbbaf57
GopalSaraf <sarafgopal37@gmail.com>
2023-04-18 23:28:59 +0530

    Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,52 @@
-import React from "react";
-import Logo from "../../images/logo/logo-no-background.png";
+import React, { useState } from "react";
 import "./Navbar.css";
 
-const Navbar = () => {
-	return (
-		<div className='nav-container'>
-			<img src={Logo} alt='error' />
-			<ul>
-				<li>
-					<a href='/login'>Login</a>
-				</li>
-				<li>
-					<a href='/register'>Register</a>
-				</li>
-			</ul>
-		</div>
-	);
-};
+function Navbar({ currentPage }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggleClick() {
+    setIsOpen(!isOpen);
+  }
+
+  return (
+    <div>
+      <div
+        className={isOpen ? "button_container active" : "button_container"}
+        id="toggle"
+        onClick={handleToggleClick}
+      >
+        <span className="top"></span>
+        <span className="middle"></span>
+        <span className="bottom"></span>
+      </div>
+      <div className={isOpen ? "overlay open" : "overlay"} id="overlay">
+        <nav className="overlay-menu">
+          <ul>
+            <li>
+              <a href="#" className={currentPage === "Home" ? "active" : ""}>
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className={currentPage === "Predict" ? "active" : ""}>
+                Predict
+              </a>
+            </li>
+            <li>
+              <a href="#" className={currentPage === "Upload" ? "active" : ""}>
+                Upload
+              </a>
+            </li>
+            <li>
+              <a href="#" className={currentPage === "Profile" ? "active" : ""}>
+                Profile
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
 
 export default Navbar;
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,201 +1,151 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import "./Register.css";
-import {Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import VisibilityIcon from "@mui/icons-material/Visibility";
-import Navbar from "../../components/Navbar/Navbar";
+import Navbar from "../../components/Navbar";
+import Navbar2 from "../../components/Navbar2";
 
 const Register = () => {
-	const [user, setUser] = useState({});
-	const [showPasswordOne, setShowPasswordOne] = useState(false);
-	const [showPasswordTwo, setShowPasswordTwo] = useState(false);
-	const [errorMsg, setErrorMsg] = useState(null);
+  const [user, setUser] = useState({});
+  const [showPasswordOne, setShowPasswordOne] = useState(false);
+  const [showPasswordTwo, setShowPasswordTwo] = useState(false);
+  const [errorMsg, setErrorMsg] = useState(null);
 
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const handleChange = (e) => {
-		const {name, value} = e.target;
-		setUser(() => {
-			return {
-				...user,
-				[name]: value,
-			};
-		});
-	};
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser(() => {
+      return {
+        ...user,
+        [name]: value,
+      };
+    });
+  };
 
-	const submit = async (e) => {
-		e.preventDefault();
-		const res = await fetch("/register", {
-			method: "POST",
-			headers: {
-				"Content-Type": "Application/json",
-			},
-			body: JSON.stringify(user),
-		});
-		console.log("data sent");
-		const data = await res.json();
-		if (res.status === 200) {
-			alert("Registration Successful");
-			navigate(`/profile/${user.RegisterAs}/${user.name}`);
-		}
-		setErrorMsg(data.message);
-	};
-	return (
-		<>
-			<div className='main-container'>
-				<div className='body-container'>
-					<div className='register-container'>
-						<h2>Signup to MediAI</h2>
-						{errorMsg ? (
-							<p className='relative top-[5px] text-[red] text-center'>
-								{errorMsg}
-							</p>
-						) : null}
-						<form>
-							<div className='input-fields'>
-								<div className='input-field'>
-									<input
-										name='name'
-										type='text'
-										placeholder='Name'
-										onChange={
-											handleChange
-										}
-									/>
-								</div>
-								<div className='input-field'>
-									<input
-										name='email'
-										type='email'
-										placeholder='Email'
-										onChange={
-											handleChange
-										}
-									/>
-								</div>
-								<div className='input-field icon-div'>
-									<input
-										name='password'
-										type={
-											showPasswordOne
-												? "text"
-												: "password"
-										}
-										placeholder='Password'
-										onChange={
-											handleChange
-										}
-									/>
-									{!showPasswordOne ? (
-										<VisibilityIcon
-											className='icons'
-											id='icon1'
-											onClick={() =>
-												setShowPasswordOne(
-													!showPasswordOne,
-												)
-											}
-										/>
-									) : (
-										<VisibilityOffIcon
-											className='icons'
-											id='icon1'
-											onClick={() =>
-												setShowPasswordOne(
-													!showPasswordOne,
-												)
-											}
-										/>
-									)}
-								</div>
-								<div className='input-field icon-div'>
-									<input
-										name='repeat_password'
-										type={
-											showPasswordTwo
-												? "text"
-												: "password"
-										}
-										placeholder='Repeat Password'
-										onChange={
-											handleChange
-										}
-									/>
-									{!showPasswordTwo ? (
-										<VisibilityIcon
-											className='icons'
-											id='icon2'
-											onClick={() =>
-												setShowPasswordTwo(
-													!showPasswordTwo,
-												)
-											}
-										/>
-									) : (
-										<VisibilityOffIcon
-											className='icons'
-											id='icon2'
-											onClick={() =>
-												setShowPasswordTwo(
-													!showPasswordTwo,
-												)
-											}
-										/>
-									)}
-								</div>
-								<div className='register-as-div'>
-									<select
-										name='RegisterAs'
-										onChange={
-											handleChange
-										}>
-										<option
-											className='select-items'
-											value='Register As'>
-											Register
-											As:
-										</option>
-										<option
-											className='select-items'
-											value='Doctor'>
-											Doctor
-										</option>
-										<option
-											className='select-items'
-											value='Patient'>
-											Patient
-										</option>
-									</select>
-								</div>
-							</div>
-							<div className='submit-btn'>
-								<input
-									onClick={
-										submit
-									}
-									type='button'
-									value='Signup'
-								/>
-							</div>
-						</form>
-						<div className='tosignin'>
-							<h4>
-								Already have an
-								account? &nbsp;
-								<Link
-									to='/login'
-									id='link'>
-									Login
-								</Link>
-							</h4>
-						</div>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+  const submit = async (e) => {
+    e.preventDefault();
+    const res = await fetch("/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "Application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    console.log("data sent");
+    const data = await res.json();
+    if (res.status === 200) {
+      alert("Registration Successful");
+      navigate(`/profile/${user.RegisterAs}/${user.name}`);
+    }
+    setErrorMsg(data.message);
+  };
+  return (
+    <>
+      <Navbar2 currentPage="Home" />
+      <div className="main-container">
+        <div className="body-container">
+          <div className="register-container">
+            <h2>Signup to MediAI</h2>
+            {errorMsg ? (
+              <p className="relative top-[5px] text-[red] text-center">
+                {errorMsg}
+              </p>
+            ) : null}
+            <form>
+              <div className="input-fields">
+                <div className="input-field">
+                  <input
+                    name="name"
+                    type="text"
+                    placeholder="Name"
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="input-field">
+                  <input
+                    name="email"
+                    type="email"
+                    placeholder="Email"
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="input-field icon-div">
+                  <input
+                    name="password"
+                    type={showPasswordOne ? "text" : "password"}
+                    placeholder="Password"
+                    onChange={handleChange}
+                  />
+                  {!showPasswordOne ? (
+                    <VisibilityIcon
+                      className="icons"
+                      id="icon1"
+                      onClick={() => setShowPasswordOne(!showPasswordOne)}
+                    />
+                  ) : (
+                    <VisibilityOffIcon
+                      className="icons"
+                      id="icon1"
+                      onClick={() => setShowPasswordOne(!showPasswordOne)}
+                    />
+                  )}
+                </div>
+                <div className="input-field icon-div">
+                  <input
+                    name="repeat_password"
+                    type={showPasswordTwo ? "text" : "password"}
+                    placeholder="Repeat Password"
+                    onChange={handleChange}
+                  />
+                  {!showPasswordTwo ? (
+                    <VisibilityIcon
+                      className="icons"
+                      id="icon2"
+                      onClick={() => setShowPasswordTwo(!showPasswordTwo)}
+                    />
+                  ) : (
+                    <VisibilityOffIcon
+                      className="icons"
+                      id="icon2"
+                      onClick={() => setShowPasswordTwo(!showPasswordTwo)}
+                    />
+                  )}
+                </div>
+                <div className="register-as-div">
+                  <select name="RegisterAs" onChange={handleChange}>
+                    <option className="select-items" value="Register As">
+                      Register As:
+                    </option>
+                    <option className="select-items" value="Doctor">
+                      Doctor
+                    </option>
+                    <option className="select-items" value="Patient">
+                      Patient
+                    </option>
+                  </select>
+                </div>
+              </div>
+              <div className="submit-btn">
+                <input onClick={submit} type="button" value="Signup" />
+              </div>
+            </form>
+            <div className="tosignin">
+              <h4>
+                Already have an account? &nbsp;
+                <Link to="/login" id="link">
+                  Login
+                </Link>
+              </h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Register;
