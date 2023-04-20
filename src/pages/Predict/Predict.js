35e3801383ff160b4fe4f7abbaebb2ad384c922d
Parth Sali <parthsali04@gmail.com>
2023-04-20 11:28:04 +0530

    update

--- src/pages/Predict/Predict.js
+++ src/pages/Predict/Predict.js
@@ -1,24 +1,58 @@
-import React, {useState} from "react";
+import React, {useState, useRef} from "react";
 import Navbar from "../../components/Navbar";
 import "./Predict.css";
+import Symptoms from "./Symptoms";
 
 const Predict = () => {
-	const [array, setArray] = useState("");
-	const submit = async () => {
-		console.log(array);
-		const res = await fetch("/predict", {
-			method: "POST",
-			headers: {
-				"Content-Type": "Application/json",
-			},
-			body: JSON.stringify(),
-		});
-		console.log("data sent");
-		const data = await res.json();
+	const [query, setQuery] = useState("");
+	const [initialSymptoms, setInitialSymptoms] = useState(Symptoms);
+	const [selectedSymptoms, setSelectedSymptoms] = useState([]);
 
-		if (data) {
-			console.log(data);
-		}
+	const submit = async () => {
+		// console.log(array);s
+		// const res = await fetch("/predict", {
+		// 	method: "POST",
+		// 	headers: {
+		// 		"Content-Type": "Application/json",
+		// 	},
+		// 	body: JSON.stringify(),
+		// });
+		// console.log("data sent");
+		// const data = await res.json();
+		// if (data) {
+		// 	console.log(data);
+		// }
+	};
+
+	const removeSymptom = (symptom) => {
+		setInitialSymptoms((prevstate) => {
+			return prevstate.filter((dataSymptom) => {
+				return symptom !== dataSymptom;
+			});
+		});
+	};
+
+	const removeSelectedSymptom = (symptom) => {
+		setSelectedSymptoms((prevstate) => {
+			return prevstate.filter((dataSymptom) => {
+				return symptom !== dataSymptom;
+			});
+		});
+		initialSymptoms.push(symptom);
+	};
+
+	const handleOnClick = (symptom) => {
+		removeSymptom(symptom);
+
+		selectedSymptoms.push(symptom);
+	};
+
+	const submitPredict = () => {
+		let symptomsArray = [];
+		selectedSymptoms.map((ele) => {
+			return symptomsArray.push(ele.name);
+		});
+		console.log(symptomsArray);
 	};
 
 	return (
@@ -39,15 +73,18 @@ const Predict = () => {
 							placeholder='Search Symptoms'
 						/>
 						<div className='symptoms-container'>
-							{Symptoms.filter(
-								(symptom) =>
-									symptom.name
-										.toLowerCase()
-										.includes(
-											query.toLowerCase(),
-										),
-							)
-								.slice(0, 12)
+							{initialSymptoms
+								.filter(
+									(
+										symptom,
+									) =>
+										symptom.name
+											.toLowerCase()
+											.includes(
+												query.toLowerCase(),
+											),
+								)
+								.slice(0, 9)
 								.map(
 									(
 										symptom,
@@ -71,7 +108,34 @@ const Predict = () => {
 								)}
 						</div>
 					</div>
-					<div className='selected-symptoms'></div>
+					<h2>Selected Symptoms</h2>
+					<div className='selected-symptoms'>
+						{selectedSymptoms.map(
+							(symptom) => {
+								return (
+									<li
+										className='symptom'
+										onClick={() => {
+											removeSelectedSymptom(
+												symptom,
+											);
+										}}
+										key={
+											symptom.name
+										}>
+										{
+											symptom.name
+										}
+									</li>
+								);
+							},
+						)}
+					</div>
+					<div className='submit-div'>
+						<button onClick={submitPredict}>
+							Predict
+						</button>
+					</div>
 				</div>
 			</div>
 		</>
